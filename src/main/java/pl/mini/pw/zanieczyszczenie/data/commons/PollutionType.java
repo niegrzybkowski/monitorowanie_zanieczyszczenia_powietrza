ab952c06a432aea55a880cb6f1e4280027ee62a9
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-16 17:23:05 +0100

    PollutionType Enum init

---
+++ src/main/java/pl/mini/pw/zanieczyszczenie/data/commons/PollutionType.java
@@ -0,0 +1,12 @@
+package pl.mini.pw.zanieczyszczenie.data.commons;
+
+public enum PollutionType {
+    STANDARD, // INDEX ONLY!
+    SO2,
+    NO2,
+    CO,
+    PM10,
+    PM25,
+    O3,
+    C6H6
+}
