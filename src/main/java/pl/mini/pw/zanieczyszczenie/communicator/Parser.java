08348d43993bc67df484c0ff34944d43b403fa1e
adrian <adrian22311@o2.pl>
2020-12-16 23:46:31 +0100

    Add Readings parser

--- src/main/java/pl/mini/pw/zanieczyszczenie/communicator/Parser.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/communicator/Parser.java
@@ -4,8 +4,13 @@ package pl.mini.pw.zanieczyszczenie.communicator;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import pl.mini.pw.zanieczyszczenie.data.commons.Station;
+import pl.mini.pw.zanieczyszczenie.data.dataclasses.Readings;
 
 
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,12 +21,13 @@ public class Parser {
     /*
     FindAll
      */
-    public List<Station> readAndParseFindAll() {
+    private String readFindAll() {
         String findAllURL_tail = "station/findAll";
         URLstring = URLstring + findAllURL_tail;
         Connection connection = new Connection(URLstring);
-        String data = connection.getData();
-
+        return connection.getData();
+    }
+    public List<Station> parseFindAll(String data) {
         List<Station> stations = new ArrayList<>();
 
         JSONArray jsonArray = new JSONArray(data);
@@ -49,6 +55,36 @@ public class Parser {
         }
         return stations;
     }
+
+
+    /*
+    getData
+     */
+    private String readReadings(int id) {
+        String getDataURL_tail = "data/getData/" + id;
+        URLstring = URLstring + getDataURL_tail;
+        Connection connection = new Connection(URLstring);
+        return connection.getData();
+    }
+    public List<Readings.Observation> parseReadings(String data) {
+        List<Readings.Observation> observations = new ArrayList<>();
+        JSONObject jsonObject = new JSONObject(data);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        JSONArray values = new JSONArray(jsonObject.get("values").toString());
+
+        for (int i=0; i<values.length(); i++) {
+            JSONObject currentvalue = values.getJSONObject(i);
+            Object value =  currentvalue.get("value");
+            String dateString = currentvalue.get("date").toString();
+            LocalDateTime date = LocalDateTime.parse(dateString, formatter);
+            if (value == JSONObject.NULL) {
+                value = BigDecimal.ZERO;
+            }
+            observations.add(new Readings.Observation(date, ((BigDecimal) value).doubleValue()));
+        }
+        return observations;
+    } // trzeba poprawic nie zapisuje key
 
 
     public static void main(String[] args) {
