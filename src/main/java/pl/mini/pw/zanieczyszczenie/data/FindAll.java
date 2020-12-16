c86379f78acb40c81e62e5a5ab8f6e5a8feefa3a
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-16 17:13:14 +0100

    lifted StationData to commons

--- src/main/java/pl/mini/pw/zanieczyszczenie/data/FindAll.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/data/FindAll.java
@@ -1,75 +1,10 @@
 package pl.mini.pw.zanieczyszczenie.data;
 
+import pl.mini.pw.zanieczyszczenie.data.commons.Station;
+
 import java.util.List;
 
 public class FindAll extends DataClass {
-    // Parsed response from GIOS API
-    public static class StationData {
-        // TODO: remove unnecessary fields
-        private int id;
-        private String stationName;
-        private double geographicLat;
-        private double geographicLon;
-        private int cityID;
-        private String cityName;
-        private String communeName;
-        private String districtName;
-        private String provinceName;
-        private String addressStreet;
-
-        public StationData(int id, String stationName, double geographicLat, double geographicLon, int cityID, String cityName, String communeName, String districtName, String provinceName, String addressStreet) {
-            this.id = id;
-            this.stationName = stationName;
-            this.geographicLat = geographicLat;
-            this.geographicLon = geographicLon;
-            this.cityID = cityID;
-            this.cityName = cityName;
-            this.communeName = communeName;
-            this.districtName = districtName;
-            this.provinceName = provinceName;
-            this.addressStreet = addressStreet;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getStationName() {
-            return stationName;
-        }
-
-        public double getGeographicLat() {
-            return geographicLat;
-        }
-
-        public double getGeographicLon() {
-            return geographicLon;
-        }
-
-        public int getCityID() {
-            return cityID;
-        }
-
-        public String getCityName() {
-            return cityName;
-        }
-
-        public String getCommuneName() {
-            return communeName;
-        }
-
-        public String getDistrictName() {
-            return districtName;
-        }
-
-        public String getProvinceName() {
-            return provinceName;
-        }
-
-        public String getAddressStreet() {
-            return addressStreet;
-        }
-    }
-
-    private List<StationData> container;
+    // findAll
+    private List<Station> container;
 }
---
+++ src/main/java/pl/mini/pw/zanieczyszczenie/data/commons/Station.java
@@ -0,0 +1,69 @@
+package pl.mini.pw.zanieczyszczenie.data.commons;
+
+public class Station {
+    // TODO: remove unnecessary fields
+    private int id;
+    private String stationName;
+    private double geographicLat;
+    private double geographicLon;
+    private int cityID;
+    private String cityName;
+    private String communeName;
+    private String districtName;
+    private String provinceName;
+    private String addressStreet;
+
+    public Station(int id, String stationName, double geographicLat, double geographicLon, int cityID, String cityName, String communeName, String districtName, String provinceName, String addressStreet) {
+        this.id = id;
+        this.stationName = stationName;
+        this.geographicLat = geographicLat;
+        this.geographicLon = geographicLon;
+        this.cityID = cityID;
+        this.cityName = cityName;
+        this.communeName = communeName;
+        this.districtName = districtName;
+        this.provinceName = provinceName;
+        this.addressStreet = addressStreet;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public double getGeographicLat() {
+        return geographicLat;
+    }
+
+    public double getGeographicLon() {
+        return geographicLon;
+    }
+
+    public int getCityID() {
+        return cityID;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCommuneName() {
+        return communeName;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public String getAddressStreet() {
+        return addressStreet;
+    }
+
+}
