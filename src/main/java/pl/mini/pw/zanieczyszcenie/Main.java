33c9f2236b9ef055eac91f78d03be4ac40f49aed
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-16 12:37:51 +0100

    test json

--- src/main/java/pl/mini/pw/zanieczyszcenie/Main.java
+++ src/main/java/pl/mini/pw/zanieczyszcenie/Main.java
@@ -1,7 +1,13 @@
 package pl.mini.pw.zanieczyszcenie;
 
+import org.json.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
+        JSONObject jooo = new JSONObject();
+        jooo.put("me", "ja");
+        jooo.put("not me", "nie ja");
+        System.out.println(jooo.toString());
     }
 }
