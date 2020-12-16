7fb18e433a5899720ca97cefbca5fa74bda14e3b
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-16 16:32:04 +0100

    init snapshot

---
+++ src/main/java/pl/mini/pw/zanieczyszczenie/data/Snapshot.java
@@ -0,0 +1,7 @@
+package pl.mini.pw.zanieczyszczenie.data;
+
+public class Snapshot extends DataClass {
+    /*
+      Class will contain everything that can be downloaded from the API for testing purposes
+     */
+}
