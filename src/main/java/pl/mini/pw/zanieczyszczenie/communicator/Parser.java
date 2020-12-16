51c90b5bef039dd00d6ca54af809a6e4e0081a6f
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-16 17:41:56 +0100

    why are we still here, just to suffer?

--- src/main/java/pl/mini/pw/zanieczyszczenie/communicator/Parser.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/communicator/Parser.java
@@ -1,4 +1,8 @@
 package pl.mini.pw.zanieczyszczenie.communicator;
 
 public class Parser {
+    /*
+        Tutaj w tym module niech będzie cała logika API,
+        powinna tworzyć obiekty z pl.mini.pw.zanieczyszczenie.data.dataclasses
+     */
 }
