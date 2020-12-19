de4420901cbdd5ca5829c51c03fc29265928d0dd
Karol <01151380@pw.edu.pl>
2020-12-19 22:56:07 +0100

    Updated Main2

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Main2.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Main2.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main2 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("app.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("app.fxml")));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
