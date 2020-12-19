ffe6e04614faf002483a7e982d15072129315dac
adrian <adrian22311@o2.pl>
2020-12-19 15:19:19 +0100

    Update Readings class

--- src/main/java/pl/mini/pw/zanieczyszczenie/data/dataclasses/Readings.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/data/dataclasses/Readings.java
@@ -1,5 +1,7 @@
 package pl.mini.pw.zanieczyszczenie.data.dataclasses;
 
+import pl.mini.pw.zanieczyszczenie.data.commons.PollutionType;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -8,8 +10,32 @@ public class Readings extends DataClass {
         Dane pomiarowe:
         pjp-api/rest/data/getData/{sensorId}
      */
-    private String key; //TODO: zmienić na enum
+    private PollutionType key; //TODO: zmienić na enum
     private List<Observation> observations;
+
+    public PollutionType getKey() {
+        return key;
+    }
+
+    public void setKey(PollutionType key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return "Readings{" +
+                "key=" + key +
+                ", observations=" + observations +
+                '}';
+    }
+
+    public List<Observation> getObservations() {
+        return observations;
+    }
+
+    public void setObservations(List<Observation> observations) {
+        this.observations = observations;
+    }
 
     public static class Observation {
         private LocalDateTime time;
@@ -17,6 +43,14 @@ public class Readings extends DataClass {
         public Observation(LocalDateTime time, double value) {
             this.time = time;
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Observation{" +
+                    "time=" + time +
+                    ", value=" + value +
+                    '}';
         }
     }
 }
