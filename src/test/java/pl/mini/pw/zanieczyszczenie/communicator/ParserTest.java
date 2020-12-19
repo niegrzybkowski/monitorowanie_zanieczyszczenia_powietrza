c45bfe49cef601e1ee8994273d42cbeca9e34e37
adrian <adrian22311@o2.pl>
2020-12-19 21:52:26 +0100

    Add basic Parser tests

--- src/test/java/pl/mini/pw/zanieczyszczenie/communicator/ParserTest.java
+++ src/test/java/pl/mini/pw/zanieczyszczenie/communicator/ParserTest.java
@@ -1,27 +1,51 @@
 package pl.mini.pw.zanieczyszczenie.communicator;
 
 import org.junit.Test;
+import pl.mini.pw.zanieczyszczenie.data.dataclasses.FindAll;
+import pl.mini.pw.zanieczyszczenie.data.dataclasses.Index;
+import pl.mini.pw.zanieczyszczenie.data.dataclasses.Readings;
+import pl.mini.pw.zanieczyszczenie.data.dataclasses.StationSensors;
+import pl.mini.pw.zanieczyszczenie.data.testData.Read;
 
 import static org.junit.Assert.*;
 public class ParserTest {
     Parser parser = new Parser();
+    Read reader = new Read();
+    String dateOfData = reader.getAvailableDates()[0];
 
     @Test
     public void parseFindAll() {
-        String data = parser.readFindAll(); //Todo przygotować folder z gotowymi plikami
+        String data = reader.readFindAll(dateOfData);
 
+        Object findAll = parser.parseFindAll(data);
 
+        assertTrue(findAll instanceof FindAll);
     }
 
     @Test
     public void parseReadings() {
+        String data = reader.readReadings(dateOfData, 92);
+
+        Object readings = parser.parseReadings(data);
+
+        assertTrue(readings instanceof Readings);
     }
 
     @Test
     public void parseStationSensors() {
+        String data = reader.readStationSensors(dateOfData, 52);
+
+        Object stationSensors = parser.parseStationSensors(data);
+
+        assertTrue(stationSensors instanceof StationSensors);
     }
 
     @Test
     public void parseGetIndex() {
+        String data = reader.readIndex(dateOfData, 52);
+
+        Object index = parser.parseGetIndex(data);
+
+        assertTrue(index instanceof Index);
     }
 }
