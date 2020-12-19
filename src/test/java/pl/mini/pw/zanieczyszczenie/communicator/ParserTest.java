03cde305e27040e5b994bf25977b2a731e787fe2
adrian <adrian22311@o2.pl>
2020-12-19 20:46:04 +0100

    Add empty test

---
+++ src/test/java/pl/mini/pw/zanieczyszczenie/communicator/ParserTest.java
@@ -0,0 +1,27 @@
+package pl.mini.pw.zanieczyszczenie.communicator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ParserTest {
+    Parser parser = new Parser();
+
+    @Test
+    public void parseFindAll() {
+        String data = parser.readFindAll(); //Todo przygotować folder z gotowymi plikami
+
+
+    }
+
+    @Test
+    public void parseReadings() {
+    }
+
+    @Test
+    public void parseStationSensors() {
+    }
+
+    @Test
+    public void parseGetIndex() {
+    }
+}
