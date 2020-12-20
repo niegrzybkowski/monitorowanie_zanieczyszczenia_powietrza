982483a45d62a7ce9feb33ce753d79770623eec9
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-20 13:43:27 +0100

    add FindAll equals, hashcode and toString

--- src/main/java/pl/mini/pw/zanieczyszczenie/data/dataclasses/FindAll.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/data/dataclasses/FindAll.java
@@ -18,4 +18,30 @@ public class FindAll extends DataClass {
     public List<Station> getContainer() {
         return container;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        FindAll findAll = (FindAll) o;
+
+        return container.equals(findAll.container);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + container.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "FindAll{" +
+                super.toString() + ", " +
+                container +
+                "}";
+    }
 }
