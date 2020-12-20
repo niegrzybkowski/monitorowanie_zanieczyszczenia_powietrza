4c7b11d342cb5fece56d7f62b4602ff9e3e7c758
Karol <01151380@pw.edu.pl>
2020-12-20 17:05:02 +0100

    add sample text to indexes

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Controller.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Controller.java
@@ -6,17 +6,45 @@ import javafx.scene.image.ImageView;
 
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class Controller {
     @FXML
     private ImageView image1;
     @FXML
     private TextField stan_powietrza;
+    @FXML
+    private TextField pm25;
+    @FXML
+    private TextField pm10;
+    @FXML
+    private TextField no2;
+    @FXML
+    private TextField co;
+    @FXML
+    private TextField c6h6;
+    @FXML
+    private TextField so2;
+
 
     public void initialize() {
         image1.setImage(new Image("obr1.png"));
         stan_powietrza.setMouseTransparent(true);
         stan_powietrza.setStyle("-fx-background-color: rgba(53,89,119,0);");
         stan_powietrza.setText("dobry");
+        var list_zan = new ArrayList<TextField>();
+        list_zan.add(pm25);
+        list_zan.add(pm10);
+        list_zan.add(no2);
+        list_zan.add(co);
+        list_zan.add(c6h6);
+        list_zan.add(so2);
+        for(TextField t : list_zan){
+            t.setMouseTransparent(true);
+            t.setStyle("-fx-background-color: rgba(53,89,119,0);");
+            t.setText("55");
+        }
+
 
     }
 
