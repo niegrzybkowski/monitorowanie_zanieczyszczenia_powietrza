6a3d300da2c37a4d2610d4aa4151699446d9feb7
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-20 14:40:37 +0100

    add basic snapshot structure

--- src/main/java/pl/mini/pw/zanieczyszczenie/data/dataclasses/Snapshot.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/data/dataclasses/Snapshot.java
@@ -1,7 +1,14 @@
 package pl.mini.pw.zanieczyszczenie.data.dataclasses;
 
+import java.util.Map;
+
 public class Snapshot extends DataClass {
     /*
       Class will contain everything that can be downloaded from the API for testing purposes
      */
+
+    private FindAll findAll;
+    private Map<Integer, StationSensors> stationSensors;
+    private Map<Integer, Readings> readings;
+    private Map<Integer, Index> index;
 }
