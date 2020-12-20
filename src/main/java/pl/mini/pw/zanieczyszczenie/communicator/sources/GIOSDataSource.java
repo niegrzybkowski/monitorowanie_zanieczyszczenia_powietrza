53c6a05946cc7426f8c74e3a5b75894a197bb93e
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-20 18:09:15 +0100

    ide spac

--- src/main/java/pl/mini/pw/zanieczyszczenie/communicator/sources/GIOSDataSource.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/communicator/sources/GIOSDataSource.java
@@ -1,6 +1,10 @@
 package pl.mini.pw.zanieczyszczenie.communicator.sources;
 
+import java.net.URL;
+
 public class GIOSDataSource implements AirQualityDataSource {
+    private static final String api = "http://api.gios.gov.pl/pjp-api/rest/";
+
     @Override
     public String getData(String pathInAPI) {
         return null;
