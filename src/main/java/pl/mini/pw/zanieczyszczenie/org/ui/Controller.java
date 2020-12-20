f43dbaa7e62a3a7b72e159f08d2472c70129a366
Karol <01151380@pw.edu.pl>
2020-12-20 16:00:17 +0100

    add Controller connection with fxml

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Controller.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Controller.java
@@ -1,4 +1,15 @@
 package pl.mini.pw.zanieczyszczenie.org.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+    @FXML
+    private ImageView image1;
+
+    public void initialize() {
+        image1.setImage(new Image("obr1.png"));
+    }
+
 }
