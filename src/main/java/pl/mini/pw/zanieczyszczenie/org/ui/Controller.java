b93c864639fee64666a072d62c23bfdc3904d50a
Karol <01151380@pw.edu.pl>
2020-12-20 16:58:00 +0100

    add sample text to stan powietrza

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Controller.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Controller.java
@@ -4,12 +4,20 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import javafx.scene.control.TextField;
+
 public class Controller {
     @FXML
     private ImageView image1;
+    @FXML
+    private TextField stan_powietrza;
 
     public void initialize() {
         image1.setImage(new Image("obr1.png"));
+        stan_powietrza.setMouseTransparent(true);
+        stan_powietrza.setStyle("-fx-background-color: rgba(53,89,119,0);");
+        stan_powietrza.setText("dobry");
+
     }
 
 }
