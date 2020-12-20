922363208b32e0f427d756e97a5bccd4174b82d1
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-20 12:54:18 +0100

    made test harder

--- src/test/java/pl/mini/pw/zanieczyszczenie/communicator/ConnectionTest.java
+++ src/test/java/pl/mini/pw/zanieczyszczenie/communicator/ConnectionTest.java
@@ -22,6 +22,6 @@ public class ConnectionTest {
 
     @Test
     public void testContents() {
-        assertTrue(data.contains("\"stationId\""));
+        assertTrue(data.contains("\"stationId\":156"));
     }
 }
