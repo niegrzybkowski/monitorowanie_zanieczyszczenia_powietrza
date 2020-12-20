c83e88eab7959f392596688e4c8de0281c6838d0
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-20 13:06:46 +0100

    add all API URLs to setup and inline Connection

--- src/test/java/pl/mini/pw/zanieczyszczenie/communicator/ConnectionTest.java
+++ src/test/java/pl/mini/pw/zanieczyszczenie/communicator/ConnectionTest.java
@@ -1,27 +1,31 @@
 package pl.mini.pw.zanieczyszczenie.communicator;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ConnectionTest {
-    static String data;
+    static String findAllResponse;
+    static String stationSensorsResponse;
+    static String readingsResponse;
+    static String indexResponse;
+
     @BeforeClass
     public static void setUp() {
-        String URLstring = "http://api.gios.gov.pl/pjp-api/rest/station/sensors/156";
-        Connection connection = new Connection(URLstring);
-        data = connection.getData();
+        findAllResponse = new Connection("http://api.gios.gov.pl/pjp-api/rest/findAll").getData();
+        stationSensorsResponse = new Connection("http://api.gios.gov.pl/pjp-api/rest/station/sensors/156").getData();
+        readingsResponse = new Connection("http://api.gios.gov.pl/pjp-api/rest/data/getData/92").getData();
+        indexResponse = new Connection("http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/52").getData();
     }
 
     @Test
     public void testConnection() {
-        assertNotNull(data);
+        assertNotNull(stationSensorsResponse);
     }
 
     @Test
     public void testContents() {
-        assertTrue(data.contains("\"stationId\":156"));
+        assertTrue(stationSensorsResponse.contains("\"stationId\":156"));
     }
 }
