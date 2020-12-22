545dba5e6e6a52ecdcd20bd9504f1f7db3f40bc1
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-22 14:34:22 +0100

    migrate to new loading method

--- src/test/java/pl/mini/pw/zanieczyszczenie/communicator/BasicParserTest.java
+++ src/test/java/pl/mini/pw/zanieczyszczenie/communicator/BasicParserTest.java
@@ -5,47 +5,44 @@ import pl.mini.pw.zanieczyszczenie.communicator.pages.FindAll;
 import pl.mini.pw.zanieczyszczenie.communicator.pages.Index;
 import pl.mini.pw.zanieczyszczenie.communicator.pages.Readings;
 import pl.mini.pw.zanieczyszczenie.communicator.pages.StationSensors;
-import pl.mini.pw.zanieczyszczenie.data.testData.Read;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 public class BasicParserTest {
-    BasicParser basicParser = new BasicParser();
-    Read reader = new Read();
-    String dateOfData = reader.getAvailableDates()[0];
+    static Parser basicParser = new BasicParser(TestUtilities::loadFromTestResources);
+
+    public static void notNullListHelper(List<?> list) {
+        assertNotNull(list);
+        assertNotEquals(0, list.size());
+        assertNotNull(list.get(0));
+    }
 
     @Test
     public void parseFindAll() {
-        String data = reader.readFindAll(dateOfData);
-
-        Object findAll = basicParser.parseFindAll(data);
-
-        assertTrue(findAll instanceof FindAll);
+        FindAll findAll = basicParser.getFindAll();
+        assertNotNull(findAll);
+        notNullListHelper(findAll.getContainer());
     }
 
     @Test
     public void parseReadings() {
-        String data = reader.readReadings(dateOfData, 92);
-
-        Object readings = basicParser.parseReadings(data);
-
-        assertTrue(readings instanceof Readings);
+        Readings readings = basicParser.getReadings(92);
+        assertNotNull(readings);
+        notNullListHelper(readings.getObservations());
     }
 
     @Test
-    public void parseStationSensors() {
-        String data = reader.readStationSensors(dateOfData, 52);
-
-        Object stationSensors = basicParser.parseStationSensors(data);
-
-        assertTrue(stationSensors instanceof StationSensors);
+    public void parseStationSensorsNotNull() {
+        StationSensors sensors = basicParser.getStationSensors(52);
+        assertNotNull(sensors);
+        notNullListHelper(sensors.getStationSensors());
     }
 
     @Test
-    public void parseGetIndex() {
-        String data = reader.readIndex(dateOfData, 52);
-
-        Object index = basicParser.parseGetIndex(data);
-
-        assertTrue(index instanceof Index);
+    public void parseGetIndexNotNull() {
+        Index index = basicParser.getIndex(52);
+        assertNotNull(index);
+        notNullListHelper(index.getIndexes());
     }
 }
