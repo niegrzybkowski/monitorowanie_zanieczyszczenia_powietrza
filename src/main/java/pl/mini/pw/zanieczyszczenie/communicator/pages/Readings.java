be06b1476f04e28145e8035add9ec24d08d1b684
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-22 17:41:35 +0100

    add todo

--- src/main/java/pl/mini/pw/zanieczyszczenie/communicator/pages/Readings.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/communicator/pages/Readings.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Readings extends APIPage {
+    // TODO: pomyśleć nad zamienieniem na mapę
     /*
         Dane pomiarowe:
         pjp-api/rest/data/getData/{sensorId}
