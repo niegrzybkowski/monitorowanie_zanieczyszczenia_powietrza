9c7bdadef38f14fad463da5e34a6fd86264998be
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2020-12-22 16:04:16 +0100

    finish up Connection refactoring

--- src/main/java/pl/mini/pw/zanieczyszczenie/communicator/Connection.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/communicator/Connection.java
@@ -1,26 +1,13 @@
 package pl.mini.pw.zanieczyszczenie.communicator;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
-import java.util.Scanner;
 
 public class Connection {
-    private java.net.URL url;
-
-    public Connection(String URLstring) {
-        try {
-            this.url = new URL(URLstring);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-    }
-
     private static HttpURLConnection getConnection(URL url) throws IOException{
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
@@ -42,30 +29,15 @@ public class Connection {
         return response.orElseThrow(()-> new IOException("Empty response and/or something has gone terribly wrong"));
     }
 
-    public String getData() {
-        try {
-            HttpURLConnection connection = getConnection(url);
-            return collectConnection(connection);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-
-    public static String readDataFromURL(String urlString) throws IOException {
+    private static String getDataFromURLThrowing(String urlString) throws IOException {
         URL url = new URL(urlString);
         HttpURLConnection connection = getConnection(url);
-        BufferedReader br = new BufferedReader(
-                new InputStreamReader(connection.getInputStream())
-        );
-        Optional<String> response = br.lines().reduce(String::concat);
-        br.close();
-        return response.orElseThrow(()-> new IOException("Empty response"));
+        return collectConnection(connection);
     }
 
     public static String getDataFromURL(String urlString) {
         try {
-            return readDataFromURL(urlString);
+            return getDataFromURLThrowing(urlString);
         } catch (IOException e) {
             e.printStackTrace();
             return "";
