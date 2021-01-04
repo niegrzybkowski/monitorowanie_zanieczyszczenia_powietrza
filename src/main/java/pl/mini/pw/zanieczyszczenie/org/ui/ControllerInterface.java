22b39bc0be74d1c47c7fbdae3fa48983b5c13138
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-04 13:14:21 +0100

    init ControllerInterface

---
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/ControllerInterface.java
@@ -0,0 +1,9 @@
+package pl.mini.pw.zanieczyszczenie.org.ui;
+
+public interface ControllerInterface {
+    // TODO: dokończyć, zastanowić się co jest jeszcze potrzebne
+    void mapClick();
+    void refreshClick();
+    void categoryClick();
+    void expandPlotClick();
+}
