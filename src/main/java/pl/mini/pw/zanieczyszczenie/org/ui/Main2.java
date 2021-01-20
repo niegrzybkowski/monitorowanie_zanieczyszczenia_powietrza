cd6423f2077b3fa506d88224b5323d403a9ac44a
Karol <01151380@pw.edu.pl>
2021-01-20 20:13:06 +0100

    proba mapy

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Main2.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/Main2.java
@@ -1,14 +1,20 @@
 package pl.mini.pw.zanieczyszczenie.org.ui;
 
+import com.gluonhq.maps.MapPoint;
+import com.gluonhq.maps.MapView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class Main2 extends Application {
+
+    private MapView mapView;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -18,6 +24,16 @@ public class Main2 extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
     }
+    private void showMap(Double lat, Double lon) {
+        mapView = new MapView();
+        PoiLayer poiLayer = new PoiLayer();
+        MapPoint mapPoint = new MapPoint(lat, lon);
+        poiLayer.addPoint(mapPoint, new Circle(7, Color.RED));
+        mapView.setZoom(16);
+        mapView.addLayer(poiLayer);
+        mapView.flyTo(0.1, mapPoint, 0.1);
+        tabMap.setContent(mapView);
+   }
 
 
     public static void main(String[] args) {
