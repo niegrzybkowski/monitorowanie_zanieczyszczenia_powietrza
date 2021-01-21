a56e57e3a78d01a05defd141f253d4902fbab72b
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-21 14:58:41 +0100

    tututututu

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/map/MapView.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/map/MapView.java
@@ -1,34 +1,31 @@
 package pl.mini.pw.zanieczyszczenie.org.ui.map;
 
-import javafx.application.Application;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
-import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MapView {
 
     private static final String IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/9/9d/Poland_CIA_map_PL.png";
-    private static final int MIN_PIXELS = 10;
-
 
     private final double width;
     private final double height;
     private final ImageView view;
+
+    private static final double longitudeLow = 14.0;
+    private static final double longitudeHigh = 25.0;
+
+    private static final double latitudeLow = 55.0;
+    private static final double latitudeHigh = 48.0;
+
+    private final List<Point2D> pois = new ArrayList<>();
 
     public MapView() {
         var img = new Image(IMAGE_URL);
@@ -36,7 +33,7 @@ public class MapView {
         this.height = img.getHeight();
         view = new ImageView(img);
         view.setPreserveRatio(true);
-        reset(view, width, height);
+        resetView();
         addListeners(view);
     }
 
@@ -45,41 +42,25 @@ public class MapView {
         ObjectProperty<Point2D> mouseDown = new SimpleObjectProperty<>();
 
         view.setOnMousePressed(e -> {
-            Point2D mousePress = imageViewToImage(view, new Point2D(e.getX(), e.getY()));
+            Point2D mousePress = viewToImage(new Point2D(e.getX(), e.getY()));
             mouseDown.set(mousePress);
         });
 
         view.setOnMouseDragged(e -> {
-            Point2D dragPoint = imageViewToImage(view, new Point2D(e.getX(), e.getY()));
-            shift(view, dragPoint.subtract(mouseDown.get()));
-            mouseDown.set(imageViewToImage(view, new Point2D(e.getX(), e.getY())));
+            Point2D dragPoint = viewToImage(new Point2D(e.getX(), e.getY()));
+            move(dragPoint.subtract(mouseDown.get()));
+            mouseDown.set(viewToImage(new Point2D(e.getX(), e.getY())));
         });
 
         view.setOnScroll(e -> {
-            double delta = e.getDeltaY();
-            Rectangle2D viewport = view.getViewport();
+            var viewport = view.getViewport();
 
-            double scale = clamp(Math.pow(1.001, delta),
-                    Math.min(MIN_PIXELS / viewport.getWidth(), MIN_PIXELS / viewport.getHeight()),
-                    Math.max(width / viewport.getWidth(), height / viewport.getHeight())
-            );
+            double scale = Math.pow(1.001, e.getDeltaY());
 
-            Point2D mouse = imageViewToImage(view, new Point2D(e.getX(), e.getY()));
+            Point2D mouse = viewToImage(new Point2D(e.getX(), e.getY()));
 
-            double newWidth = viewport.getWidth() * scale;
-            double newHeight = viewport.getHeight() * scale;
-
-            // To keep the visual point under the mouse from moving, we need
-            // (x - newViewportMinX) / (x - currentViewportMinX) = scale
-            // where x is the mouse X coordinate in the image
-
-            // solving this for newViewportMinX gives
-
-            // newViewportMinX = x - (x - currentViewportMinX) * scale
-
-            // we then clamp this value so the image never scrolls out
-            // of the imageview:
-
+            double newWidth = Math.min(viewport.getWidth() * scale, width);
+            double newHeight = Math.min(viewport.getHeight() * scale, height);
             double newMinX = clamp(mouse.getX() - (mouse.getX() - viewport.getMinX()) * scale,
                     0, width - newWidth);
             double newMinY = clamp(mouse.getY() - (mouse.getY() - viewport.getMinY()) * scale,
@@ -90,40 +71,61 @@ public class MapView {
 
         view.setOnMouseClicked(e -> {
             if (e.getClickCount() == 2) {
-                reset(view, width, height);
+                resetView();
             }
         });
     }
 
-    private void reset(ImageView imageView, double width, double height) {
-        imageView.setViewport(new Rectangle2D(0, 0, width, height));
+    private void resetView() {
+        view.setViewport(new Rectangle2D(0, 0, width, height));
     }
 
-    private void shift(ImageView imageView, Point2D delta) {
-        Rectangle2D viewport = imageView.getViewport();
+    private void move(Point2D delta) {
+        var viewport = view.getViewport();
 
-        double maxX = imageView.getImage().getWidth() - viewport.getWidth();
-        double maxY = imageView.getImage().getHeight() - viewport.getHeight();
+        double minX = clamp(viewport.getMinX() - delta.getX(), 0, width - viewport.getWidth());
+        double minY = clamp(viewport.getMinY() - delta.getY(), 0, height - viewport.getHeight());
 
-        double minX = clamp(viewport.getMinX() - delta.getX(), 0, maxX);
-        double minY = clamp(viewport.getMinY() - delta.getY(), 0, maxY);
-
-        imageView.setViewport(new Rectangle2D(minX, minY, viewport.getWidth(), viewport.getHeight()));
+        view.setViewport(new Rectangle2D(minX, minY, viewport.getWidth(), viewport.getHeight()));
     }
 
-    private double clamp(double value, double min, double max) {
+    private static double clamp(double value, double min, double max) {
         return Math.max(min, Math.min(max, value));
     }
 
-    private Point2D imageViewToImage(ImageView imageView, Point2D imageViewCoordinates) {
-        double xProportion = imageViewCoordinates.getX() / imageView.getBoundsInLocal().getWidth();
-        double yProportion = imageViewCoordinates.getY() / imageView.getBoundsInLocal().getHeight();
+    private Point2D viewToImage(Point2D viewCoordinates) {
+        double scaleX = viewCoordinates.getX() / view.getBoundsInLocal().getWidth();
+        double scaleY = viewCoordinates.getY() / view.getBoundsInLocal().getHeight();
 
-        Rectangle2D viewport = imageView.getViewport();
+        Rectangle2D viewport = view.getViewport();
         return new Point2D(
-                viewport.getMinX() + xProportion * viewport.getWidth(),
-                viewport.getMinY() + yProportion * viewport.getHeight()
+                viewport.getMinX() + scaleX * viewport.getWidth(),
+                viewport.getMinY() + scaleY * viewport.getHeight()
         );
+    }
+
+    private Point2D imageToView() {
+        return null;
+    }
+
+    private Point2D scaleGeographicToImage(double latitude, double longitude) {
+        if(!(latitudeLow <= latitude && latitude <= latitudeHigh &&
+                longitudeLow <= longitude && longitude <= longitudeHigh))
+            throw new RuntimeException("POI out of bounds in map");
+
+        double xScale = (longitudeHigh - longitudeLow) / width;
+        double yScale = (latitudeHigh - latitudeLow) / height;
+
+        double x = (longitude - longitudeLow) * xScale;
+        double y = (latitude - latitudeLow) * yScale;
+        return new Point2D(x, y);
+    }
+
+    public void addPOI(double latitude, double longitude) {
+        if(latitudeLow <= latitude && latitude <= latitudeHigh &&
+        longitudeLow <= longitude && longitude <= longitudeHigh) {
+            pois.add(new Point2D(latitude, longitude));
+        }
     }
 
     public ImageView getView() {
