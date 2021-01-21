599d0da12bc8e100d05572ba45dbe7660d0c39fc
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-21 17:09:59 +0100

    akaodmapksnmpodanm

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/map/Demo.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/map/Demo.java
@@ -2,8 +2,6 @@ package pl.mini.pw.zanieczyszczenie.org.ui.map;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -12,7 +10,8 @@ public class Demo extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         MapView mapView = new MapView();
-        mapView.addPOI(52.23, 21.01, Color.BLUE, e -> System.out.println("hello"));
+        mapView.addPOI(52.23, 21.01, Color.BLUE,
+                e -> System.out.println("tutaj byłoby coś, żeby otworzyć odpowiednie menu z prawej"));
         var pane = mapView.getPane();
         VBox root = new VBox(pane);
         stage.setScene(new Scene(root));
