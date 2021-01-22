e13580a289755f6de96c15b5ffb24eeadc88abb6
adrian <adrian22311@o2.pl>
2021-01-22 18:18:32 +0100

    Add methods

--- src/main/java/pl/mini/pw/zanieczyszczenie/model/Model.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/model/Model.java
@@ -1,4 +1,13 @@
 package pl.mini.pw.zanieczyszczenie.model;
 
+import pl.mini.pw.zanieczyszczenie.communicator.pages.*;
+
+import java.util.List;
+
 public interface Model {
+    FindAllPage getFindAll();
+    IndexPage getIndexPage(int stationId);
+    List<IndexPage> getAllIndexPages();
+    ReadingsPage getReadingsPage(int sensorId);
+    SensorsPage getSensorsPage(int stationId);
 }
