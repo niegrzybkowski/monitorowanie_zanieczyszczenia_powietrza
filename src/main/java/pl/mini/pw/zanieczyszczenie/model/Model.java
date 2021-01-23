f7e1ec2080ed9bae6cca71683d42da734a2242c1
adrian <adrian22311@o2.pl>
2021-01-23 10:27:06 +0100

    Changed model, readings read by stationID and key

--- src/main/java/pl/mini/pw/zanieczyszczenie/model/Model.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/model/Model.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface Model {
     List<StationInfoPage> getStationInfoPages();
     StationInfoPage getStationInfoPage(int stationId);
-    ReadingsPage getReadingsPage(int sensorId);
+    ReadingsPage getReadingsPage(int stationId, String key);
     SensorsPage getSensorsPage(int stationId);
 }
