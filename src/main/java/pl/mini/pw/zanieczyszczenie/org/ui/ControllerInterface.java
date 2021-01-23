9d66defe8d8fc4598b2bd4e3b91b03905c3ad1c5
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-23 15:35:40 +0100

    delete unused stuffs

--- src/main/java/pl/mini/pw/zanieczyszczenie/org/ui/ControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package pl.mini.pw.zanieczyszczenie.org.ui;
-
-public interface ControllerInterface {
-    // TODO: dokończyć, zastanowić się co jest jeszcze potrzebne
-    void mapClick();
-    void refreshClick();
-    void categoryClick();
-    void expandPlotClick();
-}
