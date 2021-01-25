99a33f622ebbc5bf1c125f453428f429c4da4b83
Karol <01151380@pw.edu.pl>
2021-01-25 18:49:58 +0100

    move Main2 to Main

--- src/main/java/pl/mini/pw/zanieczyszczenie/Main.java
+++ src/main/java/pl/mini/pw/zanieczyszczenie/Main.java
@@ -1,9 +1,28 @@
 package pl.mini.pw.zanieczyszczenie;
 
-import pl.mini.pw.zanieczyszczenie.org.ui.Main2;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+import java.util.Objects;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("main_panel.fxml")));
+        primaryStage.setTitle("Monitorowanie zanieczyszczenia powietrza");
+        primaryStage.setScene(new Scene(root, 804,  544));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+
     public static void main(String[] args) {
-        Main2.main(args);
+        launch(args);
     }
 }
